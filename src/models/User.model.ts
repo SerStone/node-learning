1f3a2341e0415dc052ab910751854f7398f39201
SerStone <mishagoal752@gmail.com>
2023-07-11 16:09:03 +0300

    add ts eslint 3

---
+++ src/models/User.model.ts
@@ -0,0 +1,36 @@
+import { model, Schema } from "mongoose";
+
+import { EGenders } from "../enums/user.enum";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    age: {
+      type: Number,
+      min: [1, "1 is min value to enter."],
+      max: [100, "100 is max value to enter."],
+    },
+    gender: {
+      type: String,
+      enum: EGenders,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const User = model("user", userSchema);
