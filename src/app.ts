e88c5d724b3f1c41701634c78f5af4ad8af12d9b
SerStone <mishagoal752@gmail.com>
2023-07-26 22:53:48 +0300

    add node cron/swagger 9

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,46 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
+import rateLimit from "express-rate-limit";
 import * as mongoose from "mongoose";
+import * as swaggerUi from "swagger-ui-express";
 
 import { configs } from "./configs/config";
+import { cronRunner } from "./crons";
 import { ApiError } from "./errors";
 import { authRouter } from "./routers/auth.router";
 import { userRouter } from "./routers/user.router";
+import * as swaggerJson from "./utils/swagger.json";
 
 const app = express();
+
+const apiLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+});
+
+app.use("*", apiLimiter);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: [
+      "Authorization",
+      "Content-Type",
+      "Origin",
+      "Access-Control-Allow-Origin",
+    ],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
   const status = err.status || 500;
@@ -23,7 +51,8 @@ app.use((err: ApiError, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-app.listen(configs.PORT, () => {
-  mongoose.connect(configs.DB_URL);
+app.listen(configs.PORT, async () => {
+  await mongoose.connect(configs.DB_URL);
+  cronRunner();
   console.log(`Server has started on PORT ${configs.PORT}`);
 });
