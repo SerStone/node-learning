cded8e6bacdd806951ee5ed9c4c415d72550cd75
SerStone <mishagoal752@gmail.com>
2023-08-02 15:43:04 +0300

    add node sockets 12

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
-const fs = require('node:fs/promises');
-const path  = require('node:path');
+const express = require('express');
+const app = express();
+const http = require('node:http');
+const socketIO = require('socket.io');
 
-const fGenMaker = async () => {
-    const mainPath = path.join(process.cwd(), 'mainFolder');
+const server = http.createServer(app);
 
-    await fs.mkdir(mainPath);
-    const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
-    const folderNames = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
+const io = socketIO(server, { cors: { origin: '*' } });
 
-    for (const file of fileNames) {
-       await fs.writeFile(path.join(mainPath, file), 'Hello OKTEN')
-    }
 
-    for (const folder of folderNames) {
-        await fs.mkdir(path.join(mainPath, folder));
-    }
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    console.log(socket.handshake);
 
-  const files = await fs.readdir(mainPath);
-    for (const file of files) {
-        const stat = await fs.stat(path.join(mainPath, file));
-        console.log(path.join(mainPath, file),' : ',stat.isDirectory() ? 'folder' : 'file');
-    }
-}
-fGenMaker();
+
+    // ---------------- ONE TO ONE -----------------------
+
+    socket.on('message:create', (messageData) => {
+        console.log(messageData, ' MESSAGE DATA');
+
+        // ONE-TO-ONE MESSAGE
+        socket.emit('message:receive', { ok: true });
+    });
+
+
+    // ---------------- BROADCAST --------------------
+
+
+    socket.on('broadcast:all', () => {
+        // // Send to all connected sockets
+        // io.emit('alert', 'Повітряна тривога');
+
+        // Send to all connected sockets EXCEPT SENDER
+        socket.broadcast.emit('alert', 'Повітряна тривога');
+    });
+
+    // ----------------- ROOMS -------------------
+
+    socket.on('room:joinUser', ({ roomId }) => {
+        socket.join(roomId); // JOIN ROOM
+        // socket.leave(roomId); // LEAVE ROOM
+
+        // // SEND TO ALL USERS IN ROOM
+        // io.to(roomId).emit('room:newUserAlert', socket.id);
+
+        // SEND TO ALL USERS IN ROOM EXCEPT SENDER
+        socket.to(roomId).emit('room:newUserAlert', socket.id);
+    })
+})
+
+server.listen(3000, () => {
+    console.log('Listen', 3000);
+});
